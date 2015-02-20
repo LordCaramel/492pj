72c6cb23ed0bb1a873deae4e7c1915f32a942e17
LordCaramel <lordcaramel@gmail.com>
2015-02-20 15:55:01 +0700

    initial

---
+++ app/src/main/java/com/example/bleble/bgapptest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.bleble.bgapptest;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public static boolean isService = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startserviceButton = (Button) findViewById(R.id.button);
+        startserviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this,BackgroundService.class));
+                Intent startMain = new Intent(Intent.ACTION_MAIN);
+                startMain.addCategory(Intent.CATEGORY_HOME);
+                startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(startMain);
+                isService = true;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        stopService(new Intent(MainActivity.this,
+                BackgroundService.class));
+        if(isService)
+        {
+            TextView tv = (TextView) findViewById(R.id.textView);
+            tv.setText("Service Resumed");
+            isService = false;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
